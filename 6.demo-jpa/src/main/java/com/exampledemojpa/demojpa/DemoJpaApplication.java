5053b7860b7d34c3b86db736de8b6356657db68f
swk802@gmail.com <swk802@gmail.com>
2022-12-11 19:13:45 +0530

    end of jpa

--- 6.demo-jpa/src/main/java/com/exampledemojpa/demojpa/DemoJpaApplication.java
+++ 6.demo-jpa/src/main/java/com/exampledemojpa/demojpa/DemoJpaApplication.java
@@ -1,5 +1,8 @@
 package com.exampledemojpa.demojpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -23,13 +26,76 @@ public class DemoJpaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 
-		Book b1 = new Book();
-		b1.setId(1);
-		b1.setCost(30);
-		b1.setName("ABC");
-		b1.setAuthorName("Rahul");
+		// 1. Inserting values through parameterized constructor
+		// Book b1 = new Book(3, "XYZ", "Ajay", 60);
+		// bookRepository.save(b1);
 
-		bookRepository.save(b1);
+		// 2. You can also do it as shown below.
+		// b1.setId(1);
+		// b1.setCost(50);
+		// b1.setName("ABC");
+		// b1.setAuthorName("Mehul");
+		// bookRepository.save(b1);
+
+
+		// 3. For inserting multiple values
+		// List <Book> books = new ArrayList<>();
+
+		// Book b1 = new Book(1, "ABC", "Rahul", 40);
+		// Book b2 = new Book(2, "XYZ", "Ajay", 60);
+
+		// books.add(b1);
+		// books.add(b2);
+		// bookRepository.saveAll(books);
+
+		// For inserting single values: save() method
+		// For inserting single values: saveAll() method
+		
+
+
+		// 4. Reading/Get data from table
+		// Use findAll() method, it return list of object.
+
+		// System.out.println(bookRepository.findAll());
+		// System.out.println(bookRepository.findByAuthorName("Rahul"));
+		// System.out.println(bookRepository.findByName("ABC"));
+		// System.out.println(bookRepository.findById(1));
+
+		// Note: It does't matter whether you write your prooerty_name starting with small or capital letter.
+		// i.e. cost or Cost, Id or id, Name or name, Authorname or authorname
+		// System.out.println(bookRepository.findByCost(40));
+		// System.out.println(bookRepository.findBycost(40));
+
+		// System.out.println(bookRepository.findById(1));
+		// System.out.println(bookRepository.findByid(1));
+
+  
+		// 5.findBy + other-name
+		// System.out.println(bookRepository.findByAuthor("Rahul"));
+		// System.out.println(bookRepository.findByAuthors("Ajay"));
+
+
+
+		// 6. For Auto generation of unique Ids, no need to pass id.
+
+		// In 'create' mode(application.properties)
+		// List <Book> books = new ArrayList<>();
+
+		// Book b1 = new Book("ABC", "Rahul", 40);
+		// Book b2 = new Book("XYZ", "Ajay", 60);
+
+		// books.add(b1);
+		// books.add(b2);
+		// bookRepository.saveAll(books);
+
+		// In 'update' mode(application.properties)
+		// Book b1 = new Book("Q21", "Jaya", 70);
+		// bookRepository.save(b1);
+
+
+		// 7. update data in table
+		
+		// 8. delete data from table
 	}
 
 }
--- 6.demo-jpa/src/main/java/com/exampledemojpa/demojpa/Models/Book.java
+++ 6.demo-jpa/src/main/java/com/exampledemojpa/demojpa/Models/Book.java
@@ -1,16 +1,27 @@
 package com.exampledemojpa.demojpa.Models;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+// @Table(name = "my_book")  // Changing the table name
 public class Book {
     
     @Id // Primary key
+    @GeneratedValue(strategy = GenerationType.AUTO) // Auto generation of unique Ids
     private int id;
 
+    // @Column(name = "book_name")   // Changing the column name
     private String name;
+
     private String authorName;
+
+    // @Column(name = "price")    // Changing the column name
     private int cost;
 
     public int getId() {
@@ -45,14 +56,32 @@ public class Book {
         this.cost = cost;
     }
 
-    public Book(String name, String authorName, int cost) {
+    public Book(int id, String name, String authorName, int cost) {
+        this.id = id;
         this.name = name;
         this.authorName = authorName;
         this.cost = cost;
     }
 
+
+    // You must have a default constructor, if you have defined a parameterized constructor.
     public Book() {
 
+    }
+
+
+    // Whenever you wanto to print the data, you need to override the toString() method.
+    @Override
+    public String toString(){
+        return "{ id = " + this.id + ", name = " + this.getName() + ", authoyName = " + this.getAuthorName() + ", cost = " + this.getCost() + " }";
+    }
+
+
+    // For Id auto generated, we nooed not to pass id 
+    public Book(String name, String authorName, int cost) {
+        this.name = name;
+        this.authorName = authorName;
+        this.cost = cost;
     }
     
 }
